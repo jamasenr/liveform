903794afed2b45cbb5d1bd7eed8dc11e41b0b3c7
jamasenr <jamasenr@gmail.com>
2024-09-20 15:45:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const backBtn = document.getElementById("back-btn");
     const progress = document.querySelector(".progress");
     let currentStep = 0;
-    const formData = {};
+    const formData = {}; 
 
     const h1 = document.querySelector("h1");
     const p = document.querySelector("p");
